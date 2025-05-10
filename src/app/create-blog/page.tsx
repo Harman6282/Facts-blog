2376432ef3169539557b3fd3836e658a3fcffe95
Harman6282 <harmans6282@gmail.com>
2025-05-10 23:39:35 +0530

    show loader while posting

--- src/app/create-blog/page.tsx
+++ src/app/create-blog/page.tsx
@@ -7,13 +7,15 @@ import { redirect } from "next/navigation";
 import { SubmitHandler, useForm } from "react-hook-form";
 import axios from "axios";
 import { toast } from "sonner";
+import { useState } from "react";
+import {Loader2} from "lucide-react"
 
 const Page = () => {
   const { status } = useSession();
-
   if (status === "unauthenticated") {
     redirect("/");
   }
+  const [posting, setPosting] = useState(false);
 
   const {
     register,
@@ -28,9 +30,11 @@ const Page = () => {
 
   const onSubmit: SubmitHandler<BlogFormData> = async (data: BlogFormData) => {
     try {
+      setPosting(true);
       const res = await axios.post("http://localhost:3000/api/blogs", data);
       console.log(res.data);
       toast.success(res.data.message);
+      setPosting(false);
       reset();
     } catch (error) {
       console.log(error);
@@ -64,9 +68,11 @@ const Page = () => {
 
         <Button
           type="submit"
-          className="bg-blue-600 text-white px-4 py-2 rounded"
+          variant="default"
+          className=" px-4 py-2 rounded" 
+          disabled={posting}
         >
-          Post
+          {posting ?  <Loader2 className="animate-spin"/>: "Post"}
         </Button>
       </form>
 
