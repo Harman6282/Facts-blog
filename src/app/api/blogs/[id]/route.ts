7883c3c2c75367cd72a5c76f89e3d4f2a860ba96
Harman6282 <harmans6282@gmail.com>
2025-05-10 22:31:53 +0530

    fix params error

--- src/app/api/blogs/[id]/route.ts
+++ src/app/api/blogs/[id]/route.ts
@@ -1,31 +1,34 @@
-import { prisma } from "@/lib/prisma";
+// import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(
   req: Request,
-  context: { params:{ id: string}}
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    const blogId =  context.params.id;
-    const blog = await prisma.blog.findUnique({
-      where: { id: blogId },
-    });
+    const blogId = (await params).id;
 
-    if (!blog) {
-      return NextResponse.json(
-        { success: false, message: "Blog not found" },
-        { status: 404 }
-      );
-    }
-
-    return NextResponse.json(
-      { success: true, message: "Blog fetched successfully", blog },
-      { status: 200 }
-    );
+    return NextResponse.json(blogId);
   } catch (error) {
     console.error("Error fetching blog:", error);
     return NextResponse.json(
       { success: false, message: "Failed to fetch blog" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const blogId = (await params).id;
+  try {
+    return NextResponse.json(blogId);
+  } catch (error) {
+    console.error("Error deleting blog:", error);
+    return NextResponse.json(
+      { success: false, message: "Failed to delete blog" },
       { status: 500 }
     );
   }
