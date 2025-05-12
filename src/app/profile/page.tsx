3225314bbb4935a3916415c5a0afcd36efb18bc4
Harman6282 <harmans6282@gmail.com>
2025-05-12 12:43:01 +0530

    added articles component and profile details

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
@@ -17,7 +19,37 @@ const Page = () => {
     return <div>Loading...</div>; // optional loader
   }
 
-  return session?.user && <div>This is Profile page</div>;
+  return (
+    session?.user && (
+      <div className="mx-auto w-screen text-xl">
+        <div className=" w-full mt-28 ">
+          <Image
+            className="rounded-full "
+            src={session?.user?.image || ""}
+            alt="Image"
+            width={50}
+            height={50}
+          />
+
+          <div>
+            <p>Id: {session?.user?.id}</p>
+            <p>Name: {session?.user?.name}</p>
+            <p>Email: {session?.user?.email}</p>
+            <p>Image: {session?.user?.image}</p>
+          </div>
+        </div>
+
+        <Button
+          className="bg-white text-black border border-black p-2 px-5 rounded cursor-pointer mt-5 hover:scale-105 hover:bg-gray-200 transform transition duration-300"
+          onClick={() => {
+            router.push("/create-blog");
+          }}
+        >
+          Post Blog
+        </Button>
+      </div>
+    )
+  );
 };
 
 export default Page;
