eec848428f017a9e96f65ed7697dc8710c0a7155
Harman6282 <harmans6282@gmail.com>
2025-05-17 14:40:40 +0530

    testing commit-2

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import Articles from "./Articles";
 
 const HomePage = () => {
-  return <div className="w-full mx-auto xl:w-[90%]">
-    <Articles />
-  </div>;
+  return (
+    <div className="w-full mx-auto xl:w-[90%]">
+      <Articles />
+    </div>
+  );
 };
 
 export default HomePage;
