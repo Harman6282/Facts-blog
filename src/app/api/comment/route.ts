5ccd44b952b69df1b162b8167298a7aed317866c
Harman6282 <harmans6282@gmail.com>
2025-05-21 14:50:16 +0530

    comment apis added

---
+++ src/app/api/comment/route.ts
@@ -0,0 +1,58 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  const { text, blogId, authorId } = await req.json();
+
+  if (!text || !blogId || !authorId) {
+    return NextResponse.json(
+      { success: false, message: "Missing fields" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const comment = await prisma.comment.create({
+      data: {
+        text,
+        blogId,
+        authorId,
+      },
+    });
+
+    return NextResponse.json(
+      { success: true, message: "Comment posted successfully", comment },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { success: false, message: "Failed to post comment" },
+      { status: 500 }
+    );
+  }
+};
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const blogId = await req.json();
+
+    const comments = await prisma.comment.findMany({
+      where: {
+        blogId,
+      },
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "Comments fetched successfully",
+      comments,
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      success: false,
+      message: "Failed to fetch comments",
+    });
+  }
+};
