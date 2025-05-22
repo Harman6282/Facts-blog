639c97a7e7a9796cc3739d724bd107e9edeba58f
Harman6282 <harmans6282@gmail.com>
2025-05-22 15:56:52 +0530

    seperate the aritcles card

---
+++ src/components/Article.tsx
@@ -0,0 +1,93 @@
+import { Heart, MessageCircle } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import DOMPurify from "dompurify";
+
+const Article = ({
+  authorId,
+  content,
+  slug,
+  authorName,
+  authorImage,
+  title,
+  imageUrl,
+  createdAt,
+  likeCount,
+  commentCount,
+}: {
+  authorId: string;
+  content: string;
+  slug: string;
+  authorName: string;
+  authorImage: string;
+  title: string;
+  imageUrl: string;
+  createdAt: Date;
+  likeCount: number;
+  commentCount: number;
+}) => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className={imageUrl ? "w-3/4" : "w-full"}>
+        <p className="text-sm inline-block mb-2 text-gray-500 ">
+          <Link
+            href={`/profile/${authorId}`}
+            className="flex items-center gap-2"
+          >
+            <Image
+              src={authorImage}
+              alt="user Image"
+              width={22}
+              height={22}
+              className="rounded-full cursor-pointer object-cover"
+            />
+            <span className=" text-black cursor-pointer hover:underline">
+              {" "}
+              {authorName}{" "}
+            </span>
+          </Link>
+        </p>
+        <Link href={`/blog/${slug}`}>
+          <h2 className="text-2xl font-bold cursor-pointer hover:underline ">
+            {title}
+          </h2>
+
+          <div
+            className="prose max-w-none text-gray-700 py-5 line-clamp-1"
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(content),
+            }}
+          />
+          <div className="text-sm text-gray-500 mt-3 flex items-center gap-4">
+            <span>
+              {" "}
+              {new Date(createdAt).toLocaleDateString("en-US", {
+                month: "short",
+                day: "numeric",
+              })}
+            </span>
+            <p className="flex items-center gap-1">
+              {" "}
+              <Heart size={16} fill="#6a7282" /> {likeCount || 0}
+            </p>
+            <p className="flex items-center gap-1">
+              {" "}
+              <MessageCircle fill="#6a7282" size={16} />
+              {commentCount || 0}
+            </p>
+          </div>
+          <hr className="my-4" />
+        </Link>
+      </div>
+
+      {imageUrl && (
+        <Link href={`/blog/${slug}`}>
+          <Image src={imageUrl} alt="blog Img" width={160} height={107}></Image>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default Article;
--- src/components/home/Articles.tsx
+++ src/components/home/Articles.tsx
@@ -1,12 +1,9 @@
 "use client";
 import axios from "axios";
-import { Heart, MessageCircle } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 import ArticlesShimmer from "../shimmer/ArticlesShimmer";
-import DOMPurify from "dompurify";
+import Article from "../Article";
 
 type Author = {
   id: string;
@@ -27,7 +24,7 @@ type Article = {
   isPublished: boolean;
   authorId: string;
   author: Author;
-  createdAt: string;
+  createdAt: Date;
   _count: {
     likes: number;
     comments: number;
@@ -61,69 +58,19 @@ const Articles = () => {
   return (
     <div className="w-full md:w-3/4 lg:w-2/3 px-6 md:px-10 p-4">
       {articles?.map((blog) => (
-        <div key={blog.id} className="flex items-center justify-between">
-          <div className={blog?.imageUrl ? "w-3/4" : "w-full"}>
-            <p className="text-sm inline-block mb-2 text-gray-500 ">
-              <Link
-                href={`/profile/${blog?.authorId}`}
-                className="flex items-center gap-2"
-              >
-                <Image
-                  src={blog.author.image}
-                  alt="user Image"
-                  width={22}
-                  height={22}
-                  className="rounded-full cursor-pointer object-cover"
-                />
-                <span className=" text-black cursor-pointer hover:underline">
-                  {" "}
-                  {blog?.author.name}{" "}
-                </span>
-              </Link>
-            </p>
-            <Link href={`/blog/${blog.slug}`}>
-              <h2 className="text-2xl font-bold cursor-pointer hover:underline ">
-                {blog.title}
-              </h2>
-
-              <div
-                className="prose max-w-none text-gray-700 py-5 line-clamp-1"
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(blog.content),
-                }}
-              />
-              <div className="text-sm text-gray-500 mt-3 flex items-center gap-4">
-                <span>
-                  {" "}
-                  {new Date(blog.createdAt).toLocaleDateString("en-US", {
-                    month: "short",
-                    day: "numeric",
-                  })}
-                </span>
-                <p className="flex items-center gap-1">
-                  {" "}
-                  <Heart size={16} fill="#6a7282" /> {blog?._count?.likes || 0}
-                </p>
-                <p className="flex items-center gap-1">
-                  {" "}
-                  <MessageCircle fill="#6a7282" size={16} />
-                  {blog?._count?.comments || 0}
-                </p>
-              </div>
-              <hr className="my-4" />
-            </Link>
-          </div>
-
-          {blog?.imageUrl && (
-            <Link href={`/blog/${blog.slug}`}>
-              <Image
-                src={blog?.imageUrl}
-                alt="blog Img"
-                width={160}
-                height={107}
-              ></Image>
-            </Link>
-          )}
+        <div key={blog?.id}>
+          <Article
+            authorId={blog?.authorId}
+            content={blog?.content}
+            slug={blog?.slug}
+            authorName={blog?.author.name}
+            authorImage={blog?.author.image}
+            title={blog?.title}
+            imageUrl={blog?.imageUrl}
+            createdAt={blog?.createdAt}
+            likeCount={blog?._count?.likes}
+            commentCount={blog?._count?.comments}
+          />
         </div>
       ))}
     </div>
